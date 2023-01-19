056249deb4e693dbdcdc7b803cea4fb0b11c27b9
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-01-19 18:35:04 +0100

    nav

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="{{ route('home') }}">LOGO</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">CHARACTERS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">COMICS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">MOVIES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">TV</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">GAMES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">COLLECTIBLES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">VIDEOS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">FANS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">NEWS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">SHOP</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
