279c271811b3a10fb31d36ded964bd4571fae147
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-01-19 23:14:14 +0100

    seeder

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -11,6 +11,18 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $array_comic = config('comic');
+
+        foreach($array_comic as $elem){
+            $new_comic = new Comic();
+            $new_comic->title = $elem['titolo'];
+            $new_comic->description = $elem['description'];
+            $new_comic->price = $elem['price'];
+            $new_comic->thumb = $elem['thumb'];
+            $new_comic->series = $elem['series'];
+            $new_comic->sale_date = $elem['sale_date'];
+            $new_comic->type = $elem['type'];
+            $new_comic->save();
+        }
     }
 }
